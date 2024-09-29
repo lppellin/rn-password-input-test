b7a1c4f1a271a14ae0807d05bbe10b610ce1ed2e
lppellin <thelucaspellin@gmail.com>
2024-09-29 15:41:09 -0300

    initial

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Pressable, TextInput, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { TogglePasswordVisibility } from "./togglePasswordVisibility"; //local onde você criou o componente
 
 export default function App() {
+  const { passwordVisibility, icon, handlePasswordVisibility } = TogglePasswordVisibility();
+  const [password, setPassword] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.inputField}
+          name="password"
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          textContentType="newPassword"
+          secureTextEntry={passwordVisibility}
+          value={password}
+          enablesReturnKeyAutomatically
+          onChangeText={setPassword}
+        />
+        <Pressable onPress={handlePasswordVisibility}>
+          <MaterialCommunityIcons name={icon} size={22} color="#232323" />
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +33,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5EEDC",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 12,
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    width: "100%",
+    borderRadius: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 4,
+    borderColor: "#d7d7d7",
+  },
+  inputField: {
+    padding: 14,
+    fontSize: 22,
+    width: "90%",
   },
 });
---
+++ togglePasswordVisibility.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const TogglePasswordVisibility = () => {
+    const [passwordVisibility, setPasswordVisibility] = useState(true);
+    const [icon, setIcon] = useState("eye");
+  
+    const handlePasswordVisibility = () => {
+      if (icon === "eye") {
+        setIcon("eye-off");
+        setPasswordVisibility(!passwordVisibility);
+      } else if (icon === "eye-off") {
+        setIcon("eye");
+        setPasswordVisibility(!passwordVisibility);
+      }
+    };
+  
+    return {
+      passwordVisibility,
+      icon,
+      handlePasswordVisibility,
+    };
+  };
+  
